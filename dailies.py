2cf82b79113c4d71704871912ca83104e7a202ff
Yadav106 <sahil.jp.yadav@gmail.com>
2024-12-20 19:32:30 +0530

    added dailies functions

--- dailies.py
+++ dailies.py
@@ -1,17 +1,238 @@
-from rich.tree import Tree
-from time import sleep
+from datetime import datetime
+from datetime import timedelta
+from rich import print
+from rich.console import Console
+from rich.prompt import Prompt
+from rich.prompt import Confirm
+from rich.table import Table
 
+
+from db import get_db_connection
 from screen import Screen
+from utils import get_positive_number
+
+console = Console()
 
 def daily_handler():
     daily_screen = Screen(
         "Dailies",
         [
-            ["create an entry", add_entry]
+            ["create an entry", add_entry],
+            ["remove an entry", remove_entry],
+            ["get entries", get_entries],
+            ["get current month stats", get_current_month_points],
+            ["get current week stats", get_current_week_points]
         ]
     )
 
 def add_entry():
-    print("Adding entry")
-    sleep(1)
+    with get_db_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, name from writers")
+        writers = cursor.fetchall()
+        writers = [[writer[0], writer[1]] for writer in writers]
 
+        writers_dict = {}
+        for writer in writers:
+            writers_dict[writer[1]] = writer[0]
+
+        writers_list = [writer[1] for writer in writers]
+        writers_list.append("c")
+
+        name = Prompt.ask("Enter writer's [blue]name[/blue]", default="enter 'c' to cancel", choices=writers_list)
+        if name.strip().lower() == 'c' or name == "enter 'c' to cancel":
+            return
+
+        writers_id = writers_dict.get(name)
+        if writers_id is None:
+            print("[red]Unexpected error occured (writers_id is none in add_entry for dailies)")
+            Prompt.ask("Press enter to continue")
+            return
+
+        try:
+            points = int(Prompt.ask("Enter points"))
+            if points < 0:
+                print("[red]points cannot be negative")
+                return
+        except Exception:
+            print(f"[red] Error occured while entering points")
+            Prompt.ask("Press enter to continue")
+            return
+
+        today = datetime.now().strftime("%d-%m-%Y")
+        date = Prompt.ask("Enter date (dd-mm-yyyy)", default=f"press enter for {today}")
+        if date == f"press enter for {today}":
+            date = today
+
+        try:
+            valid_date = datetime.strptime(date, "%d-%m-%Y")
+            valid_date.strftime("%d-%m-%Y")  # Return in the same format
+            date = valid_date.strftime("%Y-%m-%d")
+        except Exception:
+            print("[red]Invalid date format or date does not exist. Please try again.[/red]")
+            Prompt.ask("Press enter to continue")
+            return
+
+        try:
+            cursor.execute('''
+                INSERT INTO dailies (writer_id, points, date) values ((?), (?), (?))
+            ''', (writers_id, points, date))
+
+            cursor.execute('''
+                UPDATE writers
+                SET total_points = total_points + ?
+                WHERE name = ?;
+            ''', (points, name))
+            conn.commit()
+        except Exception:
+            print(f"[red]Error occured while adding daily points for user {name}")
+
+        print(f"[green]Added entry for {name}")
+        Prompt.ask("[blue]Press enter to continue")
+
+def remove_entry():
+    id = get_positive_number("Enter the id of the entry you want to remove")
+
+    try:
+        with get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('''
+                SELECT writer_id, points from dailies where id = (?)
+            ''', (id, ))
+
+            point = cursor.fetchall()
+
+            if len(point) == 0:
+                print("[red]the given id does not exist")
+                Prompt.ask("[blue]Press enter to continue")
+                return
+
+            confirmation = Confirm.ask(f"[red]do you want to delete the entry with id {id}")
+            if not confirmation:
+                print("[blue]deletion cancelled")
+                Prompt.ask("[blue]Press enter to continue")
+                return
+
+            cursor.execute('''
+                DELETE from dailies where id = (?)
+            ''', (id, ))
+
+            cursor.execute('''
+                UPDATE writers 
+                SET total_points = total_points - (?)
+                WHERE id = (?)
+            ''', (point[0][1], point[0][0]))
+
+            conn.commit()
+
+            print("[green]Entry deleted successfully!")
+            Prompt.ask("[blue]Press enter to continue")
+    except Exception as ex:
+        print(f"[red]Error while deleting entry {ex}")
+
+def get_entries():
+    n = get_positive_number('[blue]How many entries do you want to see')
+
+    try:
+        with get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('''
+                SELECT dailies.id, writers.name, dailies.date, dailies.points
+                FROM dailies
+                JOIN writers ON dailies.writer_id = writers.id
+                ORDER BY dailies.date DESC
+                LIMIT ?;
+            ''', (n, ))
+
+            daily_stats = cursor.fetchall()
+
+            table = Table(title="Daily Stats")
+            table.add_column("ID", style="cyan", no_wrap=True)
+            table.add_column("Name", style="cyan", no_wrap=True)
+            table.add_column("Date", style="cyan", no_wrap=True)
+            table.add_column("Points", style="cyan", no_wrap=True)
+
+            for stats in daily_stats:
+                table.add_row(str(stats[0]), stats[1], stats[2], str(stats[3]))
+
+            print("")
+            console.print(table)
+            print("")
+            print(f"[blue]Fetched [violet]{len(daily_stats)}[/violet] records")
+
+            Prompt.ask("[blue]Press enter to continue")
+    except Exception as ex:
+        print(f"[red]There was some issue in fetching daily entries : {ex}")
+
+def get_current_month_points():
+    try:
+        current_date = datetime.now()
+        month = current_date.month
+        year = current_date.year
+
+        first_day_of_month = f'{year}-{str(month).zfill(2)}-01'
+
+        with get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('''
+                SELECT name, SUM(points) as daily_points, date FROM writers
+                INNER JOIN dailies on dailies.writer_id = writers.id
+                WHERE date >= ?
+                GROUP BY name
+                ORDER BY daily_points DESC
+            ''', (first_day_of_month, ))
+
+            month_points = cursor.fetchall()
+
+            table = Table(title=f"Points for {current_date.strftime('%B %Y')}")
+            table.add_column("Writer", style="cyan", no_wrap=True)
+            table.add_column("Points Earned", style="cyan", no_wrap=True)
+
+            for point in month_points:
+                table.add_row(point[0], str(point[1]))
+
+            print("")
+            console.print(table)
+
+            Prompt.ask("[blue]Press enter to continue")
+
+    except Exception as ex:
+        print(f"[red]Error while fetching points for the current month: {ex}")
+        Prompt.ask("[blue]Press enter to continue")
+
+def get_current_week_points():
+    try:
+        current_date = datetime.now()
+        start_of_week = current_date - timedelta(days=current_date.weekday())  # Monday
+        end_of_week = start_of_week + timedelta(days=6)  # Sunday
+        start_of_week_str = start_of_week.strftime('%Y-%m-%d')
+        end_of_week_str = end_of_week.strftime('%Y-%m-%d')
+
+        with get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('''
+                SELECT writers.name, SUM(dailies.points) as weekly_points
+                FROM writers
+                INNER JOIN dailies ON dailies.writer_id = writers.id
+                WHERE dailies.date BETWEEN ? AND ?
+                GROUP BY writers.id
+                ORDER BY weekly_points DESC
+            ''', (start_of_week_str, end_of_week_str))
+
+            week_points = cursor.fetchall()
+
+            table = Table(title=f"Points for Week of {start_of_week.strftime('%B %d, %Y')}")
+            table.add_column("Writer", style="cyan", no_wrap=True)
+            table.add_column("Points Earned", style="cyan", no_wrap=True)
+
+            for point in week_points:
+                table.add_row(point[0], str(point[1]))
+
+            print("")
+            console.print(table)
+
+            Prompt.ask("[blue]Press enter to continue")
+
+    except Exception as ex:
+        print(f"[red]Error while fetching points for the current week: {ex}")
+        Prompt.ask("[blue]Press enter to continue")
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
+import datetime
 from os import system, name
 import sys
 from rich import print
+from rich.prompt import Prompt
 
 def sysexit():
     print("[purple]See ya soon! Tata!")
@@ -18,3 +20,21 @@ def clear():
     else:
         _ = system('clear')
 
+def convert_to_ddmmyyyy(date):
+    # Ensure the input date is a datetime object, if it's not, try to parse it
+    if isinstance(date, str):
+        date = datetime.datetime.strptime(date, "%Y-%m-%d")  # Assuming date is in "YYYY-MM-DD" format
+    
+    # Convert the date to "DD-MM-YYYY" format
+    return date.strftime("%d-%m-%Y")
+
+def get_positive_number(prompt_message):
+    while True:
+        try:
+            n = int(Prompt.ask(prompt_message))
+            if n > 0:
+                return n
+            else:
+                print("[red]The number must be greater than 0. Please try again.[/red]")
+        except ValueError:
+            print("[red]Invalid input. Please enter a positive number.[/red]")
