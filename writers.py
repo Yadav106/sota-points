2cb95e3f4813c99e1593fcbeb3012eb6e0c572b6
Yadav106 <sahil.jp.yadav@gmail.com>
2024-12-20 14:53:25 +0530

    added edit name functionality

--- writers.py
+++ writers.py
@@ -15,6 +15,7 @@ def writers_handler():
         [
             ["add a new writer", add_writer],
             ["remove a writer", remove_writer],
+            ["edit a writer's name", edit_writer_name],
             ["get all writers", get_writer_stats]
         ]
     )
@@ -54,11 +55,12 @@ def add_writer():
             print(f"[red]Error while adding a writer : [bold]{ex}[/bold]")
 
 def remove_writer():
-    name = Prompt.ask("Enter writer's [blue]name[/blue]", default="enter 'c' to cancel")
+    writers = get_all_writers()
+    writers.append("c")
+    name = Prompt.ask("Enter writer's [blue]name[/blue]", default="enter 'c' to cancel", choices=writers)
     if name.strip().lower() == 'c':
         return
 
-    writers = get_all_writers()
     if name not in writers:
         print(f"[red][bold]{name}[/bold] does not exist")
         Prompt.ask("[blue]Press enter to continue")
@@ -79,7 +81,43 @@ def remove_writer():
             print(f"[green]Writer '{name}' removed successfully![/green]")
             Prompt.ask("[blue]Press enter to continue")
         except Exception as ex:
-            print(f"[red]Error while adding a writer : [bold]{ex}[/bold]")
+            print(f"[red]Error while removing a writer : [bold]{ex}[/bold]")
+
+def edit_writer_name():
+    writers = get_all_writers()
+    writers.append("c")
+    name = Prompt.ask("Enter writer's [blue]name[/blue]", default="enter 'c' to cancel", choices=writers)
+    if name.strip().lower() == 'c':
+        return
+
+    if name not in writers:
+        print(f"[red][bold]{name}[/bold] does not exist")
+        Prompt.ask("[blue]Press enter to continue")
+        return
+
+    new_name = Prompt.ask(f"Enter new name for [blue]{name}[/blue]", default="enter 'c' to cancel")
+    if name.strip().lower() == 'c':
+        return
+
+    # Check if the new name is valid and different from the old one
+    if new_name.strip() == "" or new_name == name or new_name == "enter 'c' to cancel":
+        print("[red]New name cannot be empty or the same as the current name.[/red]")
+        Prompt.ask("[blue]Press enter to continue")
+        return
+
+    with get_db_connection() as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute('''
+                UPDATE writers SET name=(?) WHERE name=(?)
+            ''', (new_name, name,))
+            conn.commit()
+
+            print(f"[green]Writer '{name}' changed to '{new_name}'![/green]")
+            Prompt.ask("[blue]Press enter to continue")
+        except Exception as ex:
+            print(f"[red]Error while editing writer's name : [bold]{ex}[/bold]")
+
 
 def get_writer_stats():
     with get_db_connection() as conn:
@@ -98,5 +136,6 @@ def get_writer_stats():
         for writer in writers:
             table.add_row(writer[0], str(writer[1]))
 
+        print("")
         console.print(table)
         Prompt.ask("[blue]Press enter to continue")
